2ec74553682c68bb4e26fc21e92b4c8a7146af1b
Muntasir2001 <muntasirenamalhuda@gmail.com>
2023-08-23 18:52:40 +0100

    replicated homepage as much as possible

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,51 @@
+import Link from 'next/link';
+
 import styles from './page.module.scss';
+import Particles from '@/components/particles';
 
 export default function Home() {
 	return (
 		<>
-			<h2>hello</h2>
+			<div className={styles.parent}>
+				<nav className={styles.nav}>
+					<Link href='/projects' className={styles.link}>
+						Projects
+					</Link>
+					<Link href='/contact' className={styles.link}>
+						Contact
+					</Link>
+				</nav>
+				<div className={styles['glow-line']} />
+				<Particles
+					className={styles['particles-container']}
+					quantity={100}
+				/>
+				<h1 className={styles.title}>Noob Dev 54</h1>
+				<div className={styles['glow-line']} />
+				<div className={styles.body}>
+					<h2 className={styles['custom-heading']}>
+						Hi, my name is Andreas, I'm building serverless and open
+						source solutions at{' '}
+						<Link
+							target='_blank'
+							href='https://upstash.com'
+							className={styles['custom-link']}
+						>
+							Upstash
+						</Link>
+						<br />
+						and working on{' '}
+						<Link
+							target='_blank'
+							href='https://unkey.dev'
+							className={styles['custom-link']}
+						>
+							unkey.dev
+						</Link>{' '}
+						at night.
+					</h2>
+				</div>
+			</div>
 		</>
 	);
 }
