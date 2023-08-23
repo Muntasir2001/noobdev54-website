0387819e7039a749b4aa1d10a664e86c8d30f216
Muntasir2001 <muntasirenamalhuda@gmail.com>
2023-08-23 18:19:01 +0100

    changed to app router

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	compiler: {
-		styledComponents: true,
-	},
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/,
-			use: ['@svgr/webpack'],
-		});
+const nextConfig = {}
 
-		return config;
-	},
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
