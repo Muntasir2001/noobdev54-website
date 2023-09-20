ccc6bd89d96f0cd524136a5da04165e7e8acd42b
noob_dev54 <muntasirenamalhuda@gmail.com>
2023-09-20 14:23:47 +0100

    fixed the project title spacing at the top in projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,8 +12,8 @@ export default async () => {
 		<>
 			<div className='relative pb-16'>
 				<Navbar />
-				<div className='px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32'>
-					<div className='max-w-2xl mx-auto lg:mx-0'>
+				<div className='px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24'>
+					<div className='max-w-2xl mx-auto lg:mx-0 mt-5 md:mt-0'>
 						<h2 className='text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl md:mt-12'>
 							Projects
 						</h2>
